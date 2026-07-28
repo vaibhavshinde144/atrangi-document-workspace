dc1768f0467ffc65cc01d2eef7213af14352dd1a
vaibhavshinde144 <vaibhav.shinde144@gmail.com>
2026-07-28 22:30:10 +0530

    Bump Android version to 7.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.atrangi.documentworkspace"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "7.1.0"
+        versionCode = 2
+        versionName = "7.1.1"
     }
 
     buildTypes {
